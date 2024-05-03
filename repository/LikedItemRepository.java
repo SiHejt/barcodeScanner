f7933d3676e6db67b356435f2ad0dd67b1cbf52c
Byungsung4281 <164999172+Byungsung4281@users.noreply.github.com>
2024-05-03 17:35:33 +0900

    Delete repository directory

--- repository/LikedItemRepository.java
+++
@@ -1,9 +0,0 @@
-package kirin.barcodescanner.repository;
-
-import kirin.barcodescanner.Entity.LikedItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface LikedItemRepository extends JpaRepository<LikedItem, Long> {
-    List<LikedItem> findByUserId(Long userId);
-}
